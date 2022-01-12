39d3439a92b0ff9b3332701ced961b1cd6e847b8
EllieShilla <74963526+EllieShilla@users.noreply.github.com>
2022-01-12 18:43:26 +0200

    12.01.22

---
+++ TestTask Template/CSharpTest/WorkDayCalculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTest
+{
+    public class WorkDayCalculator : IWorkDayCalculator
+    {
+        public DateTime Calculate(DateTime startDate, int dayCount, WeekEnd[] weekEnds)
+        {
+            DateTime endDate = new DateTime();
+            if (weekEnds == null)
+                endDate = startDate.AddDays(dayCount - 1);
+            else
+            {
+                foreach (var date in weekEnds)
+                {
+                    System.TimeSpan diff2 = date.EndDate - date.StartDate;
+                    dayCount+= diff2.Days;
+                }
+
+                endDate = startDate.AddDays(dayCount);
+            }
+
+            return endDate;
+        }
+    }
+}
